c35752fba57caf38ce9b146cc5e78fd6a5d1e062
Prince Chauhan <bolbam13@gmail.com>
2022-12-17 22:03:46 +0530

    .

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ['bear','python3.6','peacock','kangaroo','whale','platypus']
+
+print("The animal at 1 is : ",animals[1])
+print("The third animal is : ",animals[2])
+print("The first animal is : ",animals[0])
+print("The animal at 3 is : ",animals[3])
+print("The fifth element is : ",animals[4])
+print("The animal at 2 is : ",animals[2])
+print("The sixth animal is : ",animals[5])
+print("The animal at 4 is : ",animals[4])
